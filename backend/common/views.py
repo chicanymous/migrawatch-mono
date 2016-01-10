71f89a35a1a9adab0f4c017fc99993b3b6ea39f8
chicanymous <chicanymous@gmail.com>
2016-01-09 19:55:38 -0500

    protected view mixin

--- backend/common/views.py
+++ backend/common/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic.base import View
 
-# Create your views here.
+
+class ProtectedView(View):
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(ProtectedView, self).dispatch(*args, **kwargs)
