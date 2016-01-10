372f82d53637716d190f205753a9d1fa9b79410f
chicanymous <chicanymous@gmail.com>
2016-01-09 21:06:32 -0500

    login form

---
+++ backend/accounts/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+
+class LoginForm(forms.Form):
+	email = forms.EmailField(
+		label='Email',
+		widget=forms.EmailInput(attrs={
+			'class': 'gui-input',
+			'placeholder': 'Email',
+			'autofocus': True,
+			'required': True,
+			'type': 'email'
+		}))
+	password = forms.CharField(
+		label='Email',
+		required=True,
+		widget=forms.PasswordInput(attrs={
+			'class': 'gui-input',
+			'placeholder': 'Password',
+			'autofocus': True,
+		}))
+
+	def clean_email(self):
+		return self.cleaned_data['email'].strip().lower()
+
+	def clean_password(self):
+		return self.cleaned_data['password'].strip()
+
+	def clean(self):
+		email = self.cleaned_data.get('email')
+		password = self.cleaned_data.get('password')
+
+		queryset = User.objects.filter(email=email)
+		if not queryset.exists():
+			raise forms.ValidationError('This email is not registered.')
+
+		user = queryset.first()
+		if not user.check_password(password):
+			raise forms.ValidationError('Cannot log in with these credentials.')
+		return self.cleaned_data
+
+	def get_user(self):
+		email = self.cleaned_data['email']
+		password = self.cleaned_data['password'].strip()
+		return authenticate(email=email, password=password)
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render, render_to_response, redirect
-from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, login
 from django.views.generic.base import View
+from accounts.forms import LoginForm
 
 
 class LoginView(View):
 	def get(self, request):
-		return render_to_response('dashboard/utility/login.html')
+		context = {'form': LoginForm}
+		return render(request, 'dashboard/utility/login.html', context)
+
+	def post(self, request):
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			user = form.get_user()
+			if not user:
+				return redirect('login')
+			login(request, user)
+			return redirect('dashboard:home')
+		else:
+			context = {'form': form}
+			return render(request, 'dashboard/utility/login.html', context)
 login_view = LoginView.as_view()
 
 
 class LogoutView(View):
 	def get(self, request):
 		logout(request)
-		return render_to_response('dashboard/utility/logout.html')
+		return render(request, 'dashboard/utility/logout.html')
 logout_view = LogoutView.as_view()
