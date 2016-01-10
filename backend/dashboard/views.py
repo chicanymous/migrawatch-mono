418a44ff3745ab7d838a9a615027663e6a6db75b
chicanymous <chicanymous@gmail.com>
2016-01-10 14:59:13 -0500

    request w/ user

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render
 from common.views import ProtectedView
 
 
 class HomeView(ProtectedView):
 	def get(self, request):
-		return render_to_response('dashboard/home.html')
+		return render(request, 'dashboard/home.html')
 home = HomeView.as_view()
