d978a39712a0cf5759e8f9c6f62e7f9607fd03e5
chicanymous <chicanymous@gmail.com>
2016-01-09 19:36:27 -0500

    login view (no functionality)

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.views.generic.base import View
 
-# Create your views here.
+
+class LoginView(View):
+	def get(self, request):
+		return render_to_response('dashboard/utility/login.html')
+login = LoginView.as_view()
